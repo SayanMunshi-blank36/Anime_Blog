493be52ec9024e46c10e545864fe5cec583bcd60
Sayan <sayanmunshi36@gmail.com>
2022-05-14 19:17:21 +0530

    Footer Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar /> <Component {...pageProps} />
+      <Navbar /> <Component {...pageProps} /> <Footer />
     </>
   );
 }
